3c06aa21a362914910cf89b8555fa5c3538dcbf0
Soobratty Anas <soobratty.as@gmail.com>
2022-09-23 00:42:48 +0400

    Adding eases to transitions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 gsap.defaults({ duration: 2 })
 // gsap.to('h1', { stagger: 1, x: 400, color: 'white', rotation: 360 })
-gsap.to(".fred", { x: 700, rotation: 360 });
-gsap.to(".pink", { x: 700, rotation: 360, repeat: -1, yoyo: true });
+gsap.to(".fred", { x: 700, rotation: 360, ease: "bounce" });
+gsap.to(".pink", { x: 700, rotation: 360, repeat: -1, yoyo: true, ease: "linear" });
