b16006fb50a9afac3975fce6f417e4c070a84efa
Soobratty Anas <soobratty.as@gmail.com>
2022-09-23 00:37:35 +0400

    Adding delays,yoyos and repeat

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-gsap.to('h1', { duration: 3, stagger: 1, x: 400, color: 'white', rotation: 360 })
+gsap.defaults({ duration: 2 })
+// gsap.to('h1', { stagger: 1, x: 400, color: 'white', rotation: 360 })
+gsap.to(".fred", { x: 700, rotation: 360 });
+gsap.to(".pink", { x: 700, rotation: 360, repeat: -1, yoyo: true });
