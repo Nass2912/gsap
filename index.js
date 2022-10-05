f09c79d05222941c74716c8b75f6db9a670b6036
Soobratty Anas <soobratty.as@gmail.com>
2022-10-05 10:49:34 +0400

    Added ScrollTrigger

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-gsap.defaults({ duration: 2 })
-// gsap.to('h1', { stagger: 1, x: 400, color: 'white', rotation: 360 })
-gsap.to(".fred", { x: 700, rotation: 360, ease: "bounce" });
-gsap.to(".pink", { x: 700, rotation: 360, repeat: -1, yoyo: true, ease: "linear" });
+// SETTING DEFAULTS
+gsap.defaults({ duration: 1 })
+
+let t1= gsap.timeline();
+
+//USING STAGGER AND DURATION
+// gsap.to('h1', { stagger: 1, x: 400, color: 'white', rotation: 360 }).to(".fred", { x: 700, rotation: 360, ease: "bounce" }).to(".pink", { x: 700, rotation: 360, repeat: -1, yoyo: true, ease: "linear" });
+
+t1.to("#freds img", {
+    repeat: 1, yoyo: true, ease: "linear", y: -100
+})
+t1.to("#bill img", {
+    repeat:1, yoyo:true, ease:"linear", x:100
+})
+
+gsap.to('.para', {
+    scrollTrigger:{
+        trigger: ".para",
+        markers: true,
+        scrub: 1,
+    },
+    opacity:1,
+    duration:5,
+    color:"red"
+})
+
+document.addEventListener("scroll", () => {
+    t1.reverse();
+})
